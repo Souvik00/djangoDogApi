04989f2443bc684fc4f5eedf953552ef12f3b517
souvik <souvikavro00.sa@gmail.com>
2023-07-14 11:01:04 +0600

    size choices add done in model.py

--- dogapp/models.py
+++ dogapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+SIZE_CHOICES = (
+    ('Tiny', 'Tiny'),
+    ('Small', 'Samll'),
+    ('Medium', 'Medium'),
+    ('Large', 'Large')
+)
+
+INTESNSITY_CHOICES=(
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5)
+)
